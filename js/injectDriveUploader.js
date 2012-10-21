887509b29d3fbc31b08d3b6481000eac27c7d2fd
russenreaktor <m.kupriyanov@gmail.com>
2012-10-21 12:14:41 +0200

    added oauth access token added description

--- js/injectDriveUploader.js
+++ js/injectDriveUploader.js
@@ -1,11 +1,5 @@
 console.log('injectDriveUploader');
 
-//chrome.extension.sendMessage({
-//	greeting : "hello"
-//}, function(response) {
-//	console.log(response.farewell);
-//});
-
 var FILE_TO_DOWLOAD = '';
 var FILE_BASE64 = '';
 var FILE_DATA_MIME_TYPE = '';
@@ -21,22 +15,42 @@ chrome.extension.onMessage.addListener(
     	
     	downloadFile(request.url, function(data) {
     		FILE_TO_DOWLOAD = request.url;
+    		
+    		var FILE_DESCRIPTION = 'File:' + request.url + '\nParent:' + document.location;
+    		
+    		var FILE_NAME = FILE_TO_DOWLOAD.split('/');
+    		FILE_NAME = FILE_NAME[FILE_NAME.length - 1];
     		FILE_BASE64 = data;
-    		FILE_DATA_MIME_TYPE = 'image/jpeg';
+    		FILE_DATA_MIME_TYPE = getMimeTypeByExtension(FILE_NAME);
     		
-    		(function() {
-    			var ga = document.createElement('script');
-    			ga.type = 'text/javascript';
-    			ga.async = true;
-    			ga.src = 'https://apis.google.com/js/client.js?onload=handleClientLoad';
-    			var s = document.getElementsByTagName('head')[0];
-    			s.parentNode.insertBefore(ga, s);
-    		})();
-    		
+    		insertFileOAuth(FILE_NAME, FILE_BASE64, null, FILE_DATA_MIME_TYPE, request.accessToken, FILE_DESCRIPTION);
+
     	});
     }
     
 });
+
+function getMimeTypeByExtension(url){
+	var extToMimes = {
+			'img': 'image/jpeg'
+		    , 'png': 'image/png'
+		    , 'jpg': 'image/jpeg'
+		    , 'jpeg': 'image/jpeg'
+		    , 'gif': 'image/gif'
+		    };
+
+	var ext = url.split('.');
+	   ext = ext[ext.length - 1];
+	
+	
+	ext = ext.toLowerCase();
+	
+	if (extToMimes.hasOwnProperty(ext)) {
+        return extToMimes[ext];
+	}
+    
+     return false;
+}
 
 function downloadFile(url, callback) {
 
@@ -58,7 +72,6 @@ function downloadFile(url, callback) {
 			var base64 = window.btoa(data);
 
 			callback(base64);
-			// document.getElementById("myImage").src="data:image/png;base64,"+base64;
 		}
 	};
 
@@ -66,73 +79,16 @@ function downloadFile(url, callback) {
 }
 
 
-var CLIENT_ID = '1054546345631-b4qa1umtauia8uoommptadohd2b2p0gk.apps.googleusercontent.com';
-var SCOPES = 'https://www.googleapis.com/auth/drive';
 
-  /**
-   * Called when the client library is loaded to start the auth flow.
-   */
-  function handleClientLoad() {
-    window.setTimeout(checkAuth, 1);
-  }
+function insertFileOAuth(fileDataName, fileBase64Data, fileDataType, contentMimeType, oauthToken, fileDescription, callback) {
+	console.log('insertFileOAuth', fileDataName, 'fileBase64Data', fileDataType, contentMimeType, oauthToken, fileDescription, callback);
+	const boundary = '-------314159265358979323846';
+	const delimiter = "\r\n--" + boundary + "\r\n";
+	const close_delim = "\r\n--" + boundary + "--";
 
-  /**
-   * Check if the current user has authorized the application.
-   */
-  function checkAuth() {
-    gapi.auth.authorize(
-        {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
-        handleAuthResult);
-  }
-
-  /**
-   * Called when authorization server replies.
-   *
-   * @param {Object} authResult Authorization result.
-   */
-  function handleAuthResult(authResult) {
-    if (authResult && !authResult.error) {
-//  // Access token has been successfully retrieved, requests can be sent to the API.
-	console.log('uploadFile');
-
-    gapi.client.load('drive', 'v2', function() {
-        
-        //insertFile('red_dot.png', 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==', null, 'image/png');
-        
-    	insertFile(FILE_NAME, FILE_BASE64, null, FILE_DATA_MIME_TYPE);
-        
-        
-        
-    });
-
-
-	
-} else {
-  // No access token could be retrieved, force the authorization flow.
-  gapi.auth.authorize(
-      {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
-          handleAuthResult);
-    }
-  }
-
-  /**
-   * Insert new file.
-   *
-   * @param {File} fileData File object to read data from.
-   * @param {Function} callback Function to call when the request is complete.
-   */
-//  function insertFile(fileData, callback) {
-  function insertFile(fileDataName, fileBase64Data, fileDataType, contentMimeType, callback) {
-
-	  const boundary = '-------314159265358979323846';
-const delimiter = "\r\n--" + boundary + "\r\n";
-const close_delim = "\r\n--" + boundary + "--";
-
-//var reader = new FileReader();
-//reader.readAsBinaryString(fileData);
-//reader.onload = function(e) {
   var contentType = fileDataType || 'application/octet-stream';
   var metadata = {
+    'description': fileDescription,
     'title': fileDataName,
     'mimeType': contentMimeType
   };
@@ -149,23 +105,24 @@ const close_delim = "\r\n--" + boundary + "--";
       fileBase64Data +
       close_delim;
 
-  var request = gapi.client.request({
-      'path': '/upload/drive/v2/files',
-      'method': 'POST',
-      'params': {'uploadType': 'multipart'},
-      'headers': {
-        'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-      },
-      'body': multipartRequestBody});
-      if (!callback) {
-        callback = function(file) {
-          console.log(file)
-        };
-      }
-      request.execute(callback);
-//    }
-  }
   
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://www.googleapis.com/upload/drive/v2/files', false);
+  xhr.setRequestHeader('Authorization', 'OAuth ' + oauthToken);
+  xhr.setRequestHeader('Content-Type', 'multipart/mixed; boundary="' + boundary + '"');
+  
+	xhr.onload = function(e) {
+		if (this.status == 200) {
+			console.log('done', this.response);
+		}
+	};
+
+  
+  xhr.send(multipartRequestBody);
+   	 
+  
+
+};
   
 
   
